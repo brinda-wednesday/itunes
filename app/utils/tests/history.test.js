b41e4da1ab7c71a2af6b1c1a2a7c257df762c660
Apple <apple@Apples-MacBook-Pro.local>
2021-08-31 10:39:20 +0530

    tests on history.js

--- app/utils/tests/history.test.js
+++ app/utils/tests/history.test.js
@@ -8,21 +8,30 @@ describe('tests for history(baseurl)', () => {
         exact: true
       },
       repos: {
-        route: '/github/repo',
+        route: '/github/repo/',
+        exact: true
+      },
+      trackDetails: {
+        route: '/:trackId',
         exact: true
       }
     };
   });
+
   const OLD_ENV = process.env;
   beforeEach(() => {
     jest.resetModules();
     process.env = { ...OLD_ENV };
   });
-  it('should be defined ', () => {
+  it('should be defined , by default in dev environment', () => {
     expect(getHistory).toBeDefined();
+    expect(process.env.ENVIRONMENT_NAME).not.toBe('uat');
+    expect(process.env.NODE_ENV).toBe('development');
   });
+
   it('should check env variable as uat', () => {
     process.env.ENVIRONMENT_NAME = 'uat';
-    expect(getHistory().length).toBe(1);
+    const history = getHistory();
+    expect(history.length).toBe(1);
   });
 });
